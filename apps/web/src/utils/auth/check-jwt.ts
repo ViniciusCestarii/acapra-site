b50b6b49e276b79abd083192d0083b45160b3ff8
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-03-21 16:24:16 -0300

    fix: await async on check jwt

--- apps/web/src/utils/auth/check-jwt.ts
+++ apps/web/src/utils/auth/check-jwt.ts
@@ -4,6 +4,7 @@ import * as jose from "jose";
 
 const algorithm = "ES256";
 
+// can't use node fs.readFileSync because it's not supported in the edge runtime
 const publicKey = `-----BEGIN PUBLIC KEY-----
 ${process.env.PUBLIC_TOKEN_KEY}
 -----END PUBLIC KEY-----`;
@@ -12,14 +13,14 @@ if (!publicKey) {
   throw new Error("Public key not found");
 }
 
-const ecPublicKey = await jose.importSPKI(publicKey, algorithm);
-
 export type TokenPayload = {
   id: string;
   email: string;
 };
 
 export const verifyToken = async (token: string) => {
+  const ecPublicKey = await jose.importSPKI(publicKey, algorithm);
+
   try {
     return await jose.jwtVerify<TokenPayload>(token, ecPublicKey);
   } catch (error) {
