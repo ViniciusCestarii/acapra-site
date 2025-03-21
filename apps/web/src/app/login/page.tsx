30678bc6b936139e0c51fc41d37a176969843010
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-03-21 13:31:43 -0300

    feat: add basic login page

---
+++ apps/web/src/app/login/page.tsx
@@ -0,0 +1,19 @@
+import AcapraLogo from "@/components/icons/acapra-logo";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <main className="container grid grid-cols-5 min-h-screen mx-auto py-28 px-10 gap-16">
+      <div className="col-span-2 bg-secondary flex justify-center rounded-lg py-16 px-8">
+        <AcapraLogo />
+      </div>
+      <div className="col-span-3 flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-semibold">Boas-vindas!</h1>
+        <input></input>
+        <input></input>
+      </div>
+    </main>
+  );
+};
+
+export default LoginPage;
