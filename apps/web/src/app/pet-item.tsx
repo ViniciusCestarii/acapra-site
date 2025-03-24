696bb5841149410601c7ea0662181119b3842349
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-03-24 11:20:12 -0300

    feat: add pet main image

--- apps/web/src/app/pet-item.tsx
+++ apps/web/src/app/pet-item.tsx
@@ -5,15 +5,20 @@ interface PetItem {
 }
 
 const PetItem = ({ pet }: PetItem) => {
+  const petProfileImage = pet.images.find((img) => img.id === pet.mainImageId);
   return (
     <article key={pet.id}>
       <h2>{pet.name}</h2>
       <p>{pet.sex}</p>
       <p>{pet.birthdate}</p>
       <p>{pet.observations}</p>
-      {pet.images.map((image) => (
-        <img key={image.id} src={image.src} alt={pet.name} />
-      ))}
+      {petProfileImage && (
+        <img
+          key={petProfileImage.id}
+          src={petProfileImage.src}
+          alt={pet.name}
+        />
+      )}
     </article>
   );
 };
