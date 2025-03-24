9718aeeadf7f18b134dc05343d0b7a918d923766
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-03-24 17:12:31 -0300

    feat: turn into a single component

--- apps/web/src/app/(layout-return)/layout.tsx
+++ apps/web/src/app/(layout-return)/layout.tsx
@@ -7,25 +7,15 @@ import React from "react";
 const LayoutReturn = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="sm:flex hidden fixed top-0 left-0 z-50 h-3/4 w-15 px-3 py-2 bg-secondary flex-col justify-between items-center rounded-br-lg">
+      <div className="flex flex-row-reverse sm:fixed top-0 left-0 z-50 sm:h-3/4 sm:w-15 sm:px-3 sm:py-2 sm:bg-secondary sm:flex-col sm:rounded-br-lg justify-between items-center px-10 pt-8">
         <Link href="/">
-          <AcapraLogo small />
+          <AcapraLogo small className="sm:h-auto h-7" />
         </Link>
         <Button asChild size="icon">
           <Link href="/">
             <ArrowLeft />
           </Link>
         </Button>
-      </div>
-      <div className="sm:hidden flex justify-between items-center px-10 pt-8">
-        <Button asChild size="icon">
-          <Link href="/">
-            <ArrowLeft />
-          </Link>
-        </Button>
-        <Link href="/">
-          <AcapraLogo className="h-7" small />
-        </Link>
       </div>
       <div className="sm:ml-14">
         <div className="md:max-w-3xl sm:max-w-2xl sm:mx-auto pb-8 sm:pt-8 px-10">
